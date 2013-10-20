54dc24f49598bcbeded98aa73cacd105d235a1bf
Wilco van Duinkerken <wilco@sparkboxx.com>
2013-10-20 22:06:11 +0200

    update requires of bbb

--- lib/BBB.rb
+++ lib/BBB.rb
@@ -1,8 +1,11 @@
 require "BBB/version"
 require "BBB/board"
-require "BBB/configuration"
+require "BBB/gpio"
+require "BBB/gpio/digital_pin"
 require "BBB/pin_mapper"
 require "BBB/exceptions"
+
+require "BBB/io/digital_pin"
 
 module BBB
 end
--- lib/BBB/exceptions.rb
+++ lib/BBB/exceptions.rb
@@ -1,3 +1,4 @@
 module BBB
   class UnknownPinException < ArgumentError; end
+  class UnknownPinModeException < ArgumentError; end
 end
