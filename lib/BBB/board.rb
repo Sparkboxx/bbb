e58ebfe830cea7c0105c30de0fe96dabcf7c8311
Wilco van Duinkerken <wilco@sparkboxx.com>
2013-10-20 22:05:30 +0200

    Add GPIO to board (used to be Configuration)

--- lib/BBB/board.rb
+++ lib/BBB/board.rb
@@ -1,15 +1,31 @@
 module BBB
   class Board
-    attr_reader :config
+    attr_reader :gpio
 
-    def initialize(config=nil)
-      @config = config || self.class.default_configuration
+    def initialize(gpio=nil)
+      @gpio = gpio || self.class.default_gpio
+      @pins = {}
+    end
+
+    def setup_pin(pin)
+      metaclass = class << self; self; end
+      metaclass.class_eval do
+        define_method("pin#{pin.address.to_s[1..-1]}") do
+          @pins[pin.address].value
+        end
+
+        define_method("pin#{pin.address.to_s[1..-1]}=") do |value|
+          @pins[pin.address].value=value
+        end if pin.type == :output
+      end
+      @pins[pin.address] = pin
     end
 
     private
 
-    def self.default_configuration
-      Configuration.new
+    def self.default_gpio
+      GPIO::Base.new
     end
+
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,16 +1,51 @@
 require 'spec_helper'
 
 describe BBB::Board do
+  let(:bbb) {BBB::Board.new}
+
   it "initializes with a configuration" do
-    BBB::Board.should_not_receive(:default_configuration)
+    BBB::Board.should_not_receive(:default_gpio)
     configuration = "SomeConfig"
     board = BBB::Board.new(configuration)
-    board.config.should eql(configuration)
+    board.gpio.should eql(configuration)
   end
 
   it "initializes with a default configuration" do
-    BBB::Board.should_receive(:default_configuration) { 'Default Config' }
+    BBB::Board.should_receive(:default_gpio) { 'Default Config' }
     board = BBB::Board.new
+  end
+
+  context "settings pins" do
+    Pin = Struct.new(:type, :address, :value)
+
+    it "define input function for input pin" do
+      pin = Pin.new(:input, :P8_1, 1)
+      bbb.setup_pin(pin)
+      bbb.respond_to?("pin8_1").should be_true
+    end
+
+    it "defines output function for output pin" do
+      pin = Pin.new(:output, :P8_2, 1)
+      bbb.setup_pin(pin)
+      bbb.respond_to?("pin8_2=").should be_true
+      bbb.pin8_2=0
+      pin.value.should eql(0)
+    end
+
+    it "defines input function for output pin" do
+      pin = Pin.new(:output, :P8_2, 1)
+      bbb.setup_pin(pin)
+      bbb.respond_to?("pin8_2").should be_true
+      bbb.pin8_2.should eql(1)
+    end
+
+    it "should keep the reference to the pin" do
+      pin = Pin.new(:output, :P8_2, 1)
+      bbb.setup_pin(pin)
+      pin.value=0
+      bbb.pin8_2.should eql(0)
+    end
+
   end
 
 end
