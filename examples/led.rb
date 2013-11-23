691eb22bfd1b38bf4be64c81556c0ba023d1266d
Wilco van Duinkerken <wilco@sparkboxx.com>
2013-11-23 22:37:38 +0100

    updated examples

---
+++ examples/led.rb
@@ -0,0 +1,34 @@
+require 'BBB'
+
+##
+# Setup the LED Circuit
+#
+class Circuit < BBB::Circuit
+  def initialize
+    # Attach the led to pin P8_10
+    attach BBB::Components::Led, pin: :P8_10, as: :led
+  end
+end
+
+##
+# Setup the actual Application
+#
+class LedExampleApplication < BBB::Application
+  # Run this on the BeableBoneBlack
+  board BBB::Board.new
+
+  # Connection the led circuit
+  circuit LedExample::Circuit.new
+
+  # This is the basic run loop
+  def run
+    led.on!  # this does what you think it does
+    sleep(1) # sleep for 1 second, kind of blunt, since it blocks everthing.
+    led.off!
+    sleep(1)
+  end
+end
+
+# Initialize and run the LedExampleApplication
+app = LedExampleApplication.new
+app.start
--- examples/sketches.rb
+++ examples/sketches.rb
@@ -10,7 +10,7 @@ module Thunderball
     ##
     # Load the thunderball layout
     #
-    layout Layout.new
+    layout Thunderball::Circuit.new
 
     attr_reader :stabalizer, :mover
 
@@ -45,22 +45,21 @@ module Thunderball
     end
   end # Copter
 
-  class Layout < BBB::Layout
+  class Circuit < BBB::Circuit
     def initialize
       attach_escs
       attach_led
     end
 
     def attach_escs
-      attach ESC, :pins=>[:P8_1, :P8_2], :as=>:esc_1
-      attach ESC, :pins=>[:P9_1, :P9_2], :as=>:esc_2
+      attach ESC, :pins=>[:P8_1, :P8_2], :as=>:esc_1, :group=>:escs
+      attach ESC, :pins=>[:P9_1, :P9_2], :as=>:esc_2, :group=>:escs
     end
 
     def attach_leds
-      attach Led.new(option: "foo",
-                     option2: "bar"), :pin=>:P9_12
+      attach Led, :pin=>:P9_12
     end
-  end # Layout
+  end # Circuit
 
   class Mover
     def initialize(opts={})
@@ -84,185 +83,4 @@ module Thunderball
       # and update all values
     end
   end #Stabalizer
-end
-
-module BBB
-  class Layout
-    ##
-    # include default components like Servo and Led
-    #
-    include Components
-
-    ##
-    # @param component [Object, Class] the component to attach. If an Object is
-    # given it will attach is as is, if a class is given, a new instance of the
-    # class will be attached.
-    #
-    # @param [Hash] The options for naming and pins
-    # @option opts [Symbol]         :as           name of the component
-    # @option opts [Array<Symbol>]  :pins         input pins for the component, alias of pins
-    # @option opts [Array<Symbol>]  :input_pins   input pins for the component
-    # @option opts [Array<Symbol>]  :output_pins  output pins for the component
-    # @option opts [Symbol]         :group        place component in group
-    #
-    def attach(component, options)
-
-    end
-  end # BBB::Layout
-
-  class Application
-    board BBB::Board.new
-
-    def initialize
-    end
-
-    def start
-      loop do
-        run
-      end
-    end
-
-    def run
-      raise NotImplementedError
-    end
-
-    def board
-      self.class.board
-    end
-
-    def layout(layout)
-      self.class.board.setup(:layout=>layout)
-    end
-
-    def self.board(board)
-      @board = board
-    end
-
-  end # BBB::Application
-
-  class Board
-
-    def read
-      input_pins.each(&:read)
-    end
-
-    def write
-      output_pins.each(&:write)
-    end
-
-    def setup(layout)
-
-    end
-
-  end # BBB::Board
-
-  class Pin
-    def self.factory(klass=:digital, type=:output, opts={})
-      const = "#{klass.capitalize}::#{type.capitalize}"
-      const_get(const).new(opts)
-    end
-
-    class Digital
-
-      class Pin
-        attr_reader :value
-
-        def initialize(opts={})
-        end
-      end
-
-      class Input < Pin
-        def read
-        end
-
-        def high?
-          value == 1
-        end
-
-        def low?
-          value == 0
-        end
-
-        def value
-          read
-        end
-      end
-
-      class Output < Input
-        def write
-
-        end
-
-        def high
-          value = 1
-        end
-
-        def low
-          value = 0
-        end
-
-        private
-
-        def value=(value)
-          @value = value
-          write
-        end
-      end
-
-    end
-  end
-
-  module Components
-    class Component
-      attr_accessor :input_pins, :output_pins, :input_pin
-
-      ##
-      # Set the readers and writers of the component
-      # @param [Hash] The options for the input and output pins
-      # @option opts [Array<Symbol>]  :pins         input pins for the component, alias of pins
-      # @option opts [Array<Symbol>]  :input_pins   input pins for the component
-      # @option opts [Array<Symbol>]  :output_pins  output pins for the component
-      #
-      def attach(opts={})
-        opts.each do |key, value|
-          send("#{key}=".to_sym, value)
-        end
-      end
-
-      def pins=(pins)
-        input_pins = pins
-      end
-
-      def pin=(pin)
-        input_pin = pin
-      end
-    end
-
-    class Led < Component
-
-      def on
-        input_pin.high
-      end
-
-      def on?
-        input_pin.high?
-      end
-
-      def off
-        input_pin.low
-      end
-
-      def off
-        input_pin.low?
-      end
-
-      def pins=(pins)
-        input_pin = pins.first
-      end
-
-    end
-
-    class Servo < Component; end
-    class ESC < Servo; end
-  end
 end
