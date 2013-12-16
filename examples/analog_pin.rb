b75ec079a9f7e139100325a5339b12e36de81c70
Wilco van Duinkerken <wilco@sparkboxx.com>
2013-12-16 23:01:34 +0100

    add documentation to the example

--- examples/analog_pin.rb
+++ examples/analog_pin.rb
@@ -1,3 +1,23 @@
+#
+# To run this example do this first:
+#
+# Make sure you run as root
+# > sudo su
+#
+# Install BBB gem version 0.0.9 or higher
+# > gem install BBB
+#
+# Then activate the ADC using the cape
+# > echo cape-bone-iio > /sys/devices/bone_capemgr.*/slots
+#
+# No open up an IRB console and copy paste in this code
+# > irb
+#
+# BE CAREFUL:
+# The AIN pins only take 1.8V at max. So if you connect an output pin
+# to an input pin make sure you use a voltage divider circuit. See here:
+# http://learn.adafruit.com/setting-up-io-python-library-on-beaglebone-black/adc
+#
 require 'BBB'
 
 ##
