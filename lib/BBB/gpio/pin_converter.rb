68b03a275b17bc28c1d728bfafce288dc34b509b
Wilco van Duinkerken <wilco@sparkboxx.com>
2013-12-16 22:29:41 +0100

    Fix issue in pin_converter

--- lib/BBB/gpio/pin_converter.rb
+++ lib/BBB/gpio/pin_converter.rb
@@ -11,14 +11,16 @@ module BBB
       # Currently only converts IO::DigitalPins
       #
       def convert(pin, opts={})
+        opts = {:mock=>mock}.merge(opts)
+
         base_class = pin.class.to_s.split("::").last
         if GPIO.const_defined?(base_class)
           klass = GPIO::const_get(base_class.to_sym)
+          return klass.new(pin.position, pin.mode, opts)
         elsif ADC.const_defined?(base_class)
           klass = ADC::const_get(base_class.to_sym)
+          return klass.new(pin.position, opts)
         end
-        opts = {:mock=>mock}.merge(opts)
-        return klass.new(pin.position, pin.mode, opts)
       end
 
     end
