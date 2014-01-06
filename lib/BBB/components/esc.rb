6d7a1935d576a4db077588d8c7753bfc3c16d3ac
Wilco van Duinkerken <wilco@sparkboxx.com>
2014-01-06 13:30:19 +0100

    beginnings of an ESC class.

---
+++ lib/BBB/components/esc.rb
@@ -0,0 +1,46 @@
+module BBB
+  module Components
+    class ESC
+      include Pinnable
+      uses Pins::PWMPin, Pins::DigitalOutputPin
+
+      attr_accessor :min_duty, :max_duty, :period
+      attr_reader :duty
+
+      def initialize(period=10e6, min_duty=0.8e6, max_duty=2e6)
+        @period   = period
+        @min_duty = min_duty
+        @max_duty = max_duty
+      end
+
+      def after_pin_initialization
+        power.off!
+        pwm.period = period
+        pwm.duty   = min_duty
+        pwm.run    = 1
+      end
+
+      def speed(value)
+        self.duty = min_duty + value * duty_range
+      end
+
+      def duty=(value)
+        @duty = value
+        pwm.duty = @duty
+      end
+
+      def duty_range
+        max_duty - min_duty
+      end
+
+      def pwm
+        pins.first
+      end
+
+      def power
+        pins.last
+      end
+
+    end
+  end
+end
